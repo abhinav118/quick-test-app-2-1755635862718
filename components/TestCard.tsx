73531846943575999b07c2c0e843f6341b5ce79a
Abhi <abhinav118@gmail.com>
2025-08-19 16:37:48 -0400

    Add components/TestCard.tsx

---
+++ components/TestCard.tsx
@@ -0,0 +1,14 @@
+interface TestCardProps {
+  title?: any
+  status?: any
+  count?: any
+  clickable?: any
+}
+
+export default function TestCard({ title, status, count, clickable }: TestCardProps) {
+  return (
+    <div className="">
+      {/* TestCard component content */}
+    </div>
+  )
+}
