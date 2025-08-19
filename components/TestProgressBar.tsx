b67ae3e30aef09c33b84496857036b384d4c72f0
Abhi <abhinav118@gmail.com>
2025-08-19 16:37:49 -0400

    Add components/TestProgressBar.tsx

---
+++ components/TestProgressBar.tsx
@@ -0,0 +1,13 @@
+interface TestProgressBarProps {
+  progress?: any
+  total?: any
+  label?: any
+}
+
+export default function TestProgressBar({ progress, total, label }: TestProgressBarProps) {
+  return (
+    <div className="">
+      {/* TestProgressBar component content */}
+    </div>
+  )
+}
